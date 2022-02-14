fa262f0e8193c81181625e3d8b7da4c9e890b9dc
Sokratis Anagnostopoulos <65159898+soanagno@users.noreply.github.com>
2022-02-14 14:48:17 +0100

    Add files via upload

--- cleanplot.py
+++ cleanplot.py
@@ -1,3 +1,4 @@
+import json
 import numpy as np
 from matplotlib import cm
 import matplotlib.pyplot as plt
@@ -9,38 +10,55 @@ class interface():
     def __init__(self):
 
 
+        # Open JSON file
+        plot_info = open("plot_info.json",)
+        # returns JSON object as a dictionary
+        data = json.load(plot_info)
+        # Close JSON file
+        plot_info.close()
+
         # Initialise figure
         self.fig, self.ax = plt.subplots()
 
         # Public
-        self.dpi = 1200
-        self.save_fig = True
-        self.saveas = 'figure.tiff'
-        self.title = 'cos(θ)'
-        self.xlabel = 'x axis'
-        self.ylabel = 'y axis'
-        self.font = 'sans-serif'
-        # self.cmaps = ['r', 'b']
+        self.dpi = data['general']['dpi']
+        self.save_fig = data['general']['save_fig']
+        self.saveas = data['general']['saveas']
+        self.title = data['general']['title']
+        self.xlabel = data['general']['xlabel']
+        self.ylabel = data['general']['ylabel']
+        self.font = data['general']['font']
+        self.legends = data['general']['legends']
+        self.cmaps = data['general']['cmaps']
         # self.cmaps = ['twilight_shifted_r', 'viridis']
-        self.cmaps = ['twilight_shifted_r', 'plasma']
+        # self.cmaps = ['twilight_shifted_r', 'plasma']
         # self.cmaps = ['twilight_shifted_r', 'magma', 'inferno_r', 'twilight']
-        self.ms = 4
-        self.c_spec = 1
+        self.ms = data['general']['ms']
+        self.c_spec = data['general']['c_spec']
         # self.linestyle = [':', '-.', '--', '-']
         # self.marker = ['^', 'o', 'x', 'v']
-        self.linestyle = None
-        self.marker = None
+        self.linestyle = data['general']['linestyle']
+        self.marker = data['general']['marker']
 
 
-    def setup(self, x, y, true_scale=True, aspect=None, domain=None):
+    def setup(self, x, y, true_scale=True, aspect=None, domain=None, c=None):
         
+        # Set specific colors
+        if c != None:
+            self.cmaps = c
+
         # Plots size
-        n = x.shape[0]
+        if x.ndim == 1:
+            self.n = 1
+            x = x.reshape((1, -1))
+            y = y.reshape((1, -1))
+        elif x.ndim == 2:
+            self.n = x.shape[0]
 
         if self.linestyle == None:
-            self.linestyle = [None]*n
+            self.linestyle = [None]*self.n
         if self.marker == None:
-            self.marker = [None]*n
+            self.marker = [None]*self.n
 
         # Parameters
         # plt.rc('lines', linewidth=2)
@@ -64,12 +82,8 @@ class interface():
         # Create custom map instance
         customap = ListedColormap(newcolors, name='customap')
         cmap = plt.get_cmap(customap)
-        colors = cmap(np.linspace(0, 1*self.c_spec, n)) # get n colors from cmap
+        colors = cmap(np.linspace(0, 1*self.c_spec, self.n)) # get n colors from cmap
         self.ax.set_prop_cycle(color=colors)     # set colors to cycle prop
-
-        # # Plot graphs
-        # for i in range(n):
-        #     plt.plot(x[i], y[i])
 
         # Define domain dimensions
         if domain == None:
@@ -99,22 +113,14 @@ class interface():
         elif aspect != None:
             self.ax.set_aspect(aspect*xdif/ydif)
 
-        # Ticks and labels
-        # plt.tight_layout()
-        self.ax.tick_params(direction="in")
-        plt.xlabel(self.xlabel)
-        plt.ylabel(self.ylabel)
-        plt.title(self.title)
+        return x, y
 
 
-    def plot(self, x, y):
+    def plot(self, x, y, c=None, true_scale=False):
 
-        # Plots size
-        n = x.shape[0]
-
-        self.setup(x, y)
-        for i in range(n):
-            plt.plot(x[i], y[i], linestyle=self.linestyle[i], marker=self.marker[i], ms=self.ms)
+        x, y = self.setup(x, y, c=c, true_scale=true_scale)
+        for i in range(self.n):
+            self.ax.plot(x[i], y[i], linestyle=self.linestyle[i], marker=self.marker[i], ms=self.ms)
 
         # Save figure
         # save as tiff -> word document -> save as png
@@ -122,14 +128,11 @@ class interface():
             self.fig.savefig(self.saveas, dpi=self.dpi)
 
 
-    def scatter(self, x, y):
+    def scatter(self, x, y, c=None, true_scale=False):
 
-        # Plots size
-        n = x.shape[0]
-
-        self.setup(x, y)
-        for i in range(n):
-            plt.scatter(x[i], y[i], s=3)
+        x, y = self.setup(x, y, c=c, true_scale=true_scale)
+        for i in range(self.n):
+            self.ax.scatter(x[i], y[i], s=3)
 
         # Save figure
         # save as tiff -> word document -> save as png
@@ -138,15 +141,19 @@ class interface():
 
 
     def show(self,):
+
+        # Ticks and labels
+        # plt.tight_layout()
+        self.ax.tick_params(direction="in")
+        plt.xlabel(self.xlabel)
+        plt.ylabel(self.ylabel)
+        plt.title(self.title)
+        if self.legends != None:
+            plt.legend(self.legends)
         plt.show()
 
 
-    def forceAspect(ax, aspect=1):
-        im = ax.get_images()
-        extent = im[0].get_extent()
-        ax.set_aspect(abs((extent[1]-extent[0])/(extent[3]-extent[2]))/aspect)
-
-# Todo 
+# Todo:
 # 2d plots, 2d scatter, 2d contour
 # 2d plot with points
 # 3d plots, 3d scatter, 3d contour
--- example_main.py
+++ example_main.py
@@ -4,12 +4,18 @@ import numpy as np
 from cleanplot import interface
 cplt = interface()
 
-def main()
+if __name__ == "__main__":
+
+    # # Create data
+    # X = np.random.uniform(low=0.01, high=0.02, size=(1000,))
+    # x = np.sort(X)
+    # y = np.cos(1/x)/x
+    # cplt.scatter(x, y, true_scale=False)
+    # cplt.show()
 
     X = np.linspace(0, 2*np.pi, 100)
     # Y = X**2/np.cos(X)+1
     Y = np.cos(X)
-
     n = 40
     x = np.zeros((n, X.size))
     y = np.zeros((n, Y.size))
@@ -20,10 +26,5 @@ def main()
         else:
             x[i] = x[i-1]+10/n
 
-    cplt.plot(x, y)
+    cplt.plot(x, y, true_scale=True)
     cplt.show()
-
-    
-if __name__ == "__main__":
-    main()
-    
